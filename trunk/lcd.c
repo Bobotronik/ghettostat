57b1a966e029f00549a371b564a753bbf0a174c5
jammyo40 <jammyo40@57e272f0-0f25-a5b4-1685-e8bbf2b11246>
2010-03-23 21:39:08 +0000

    working

--- trunk/lcd.c
+++ trunk/lcd.c
@@ -1,7 +1,7 @@
 #include "derivative.h"
 #include "lcd.h"
 
-void initializeCharMap(){
+/*void initializeCharMap(){
   charMap['0'] = 0x;
   charMap['1'] = 0x;
   charMap['2'] = 0x;
@@ -16,30 +16,21 @@ void initializeCharMap(){
   charMap['b'] = 0x;
   charMap['c'] = 0x;
   charMap['d'] = 0x;
-}
+}   */
 
 void wait(unsigned char cycles){
-  for (;cycles == 0; cycles--){
+  for (;cycles != 0; cycles--){
   
   }
 }
 
-unsigned char readStatus(){    
+unsigned char readStatus(){
+  unsigned int status;
   DDRB = 0x00;
   
-  /*CD = 1;
-  RD = 0;
-  WR = 1;
-  CE = 0;
-  wait(1);
-  while (PTB_PTB0 != 1 || PTB_PTB1 != 1){
-    //return true; 
-  }
-  CE = 1;  */
   CD = 1;
   RD = 0;
   WR = 1;
-  unsigned char status;
   
   do{
     CE = 0;
@@ -48,7 +39,7 @@ unsigned char readStatus(){
     CE = 1;    
     status &= 0x03;     
   } while(status != 0x03);
-  
+           
   return 1;
 }
 
@@ -64,6 +55,7 @@ void writeData(unsigned char data){
 }
 
 unsigned char readData(){
+  unsigned char data;
   readStatus();
   DDRB = 0x00;
   CD = 0;
@@ -71,7 +63,7 @@ unsigned char readData(){
   WR = 1; 
   CE = 0; 
   wait(1);
-  unsigned char data = PTB;
+  data = PTB;
   CE = 1;
   
   return data; 
@@ -96,11 +88,11 @@ void setADP(unsigned short int ADP){
 
 void display(unsigned char data){
   writeData(data);
-  writeCommand(WRITE_DATA_AND_INCREMENT);
+  writeCommand(DATA_WRITE_AND_INCREMENT);
 }
 
-void printChar(char character)[
-  display(char - 32);
+void printChar(char character){
+  display(character - 32);
 }
 
 void printStr(char* string){
@@ -119,18 +111,35 @@ void goToText(unsigned char x, unsigned char y){
   setADP(address);
 }
 
-void goToGraphics(unsigned char x, unsigned char y){
+void goToGraphic(unsigned char x, unsigned char y){
   unsigned short int address = GRAPHIC_HOME + GRAPHIC_AREA*y + x/FONT_WIDTH;
   setADP(address);
 }
 
+void clearText(){
+  short unsigned int i;
+  setADP(TEXT_HOME);  
+  for (i = TEXT_SIZE; i != 0; i--){
+    display(0x00); 
+  }
+}
+
+void clearGraphic(){
+  short unsigned int i;
+  setADP(GRAPHIC_HOME);
+  for (i = GRAPHIC_SIZE; i != 0; i--){
+    display(0x00); 
+  } 
+}
+      
 void initializeDisplay(){
 
   // Set control lines as output
   DDRA_DDRA0 = 1;
   DDRA_DDRA1 = 1;
   DDRA_DDRA4 = 1;
-  DDRA_DDRA5 = 1;  
+  DDRA_DDRA5 = 1; 
+  DDRD_DDRD4 = 1; 
   
   // Reset
   PTA_PTA5 = 0;
@@ -138,20 +147,21 @@ void initializeDisplay(){
   PTA_PTA5 = 1;
   wait(10);
 
-  writeData(TEXT_HOME_ADDRESS & 0xff);
-  writeData(TEXT_HOME_ADDRESS >> 8);
+  writeData(GRAPHIC_HOME & 0xff);
+  writeData(GRAPHIC_HOME >> 8); 
+  writeCommand(SET_GRAPHIC_HOME_ADDRESS); 
+
+  writeData(GRAPHIC_AREA);
+  writeData(0x00);
+  writeCommand(SET_GRAPHIC_AREA); 
+  
+  writeData(TEXT_HOME & 0xff);
+  writeData(TEXT_HOME >> 8);
   writeCommand(SET_TEXT_HOME_ADDRESS);
 
   writeData(TEXT_AREA);
   writeData(0x00);
-  writeCommand(SET_TEXT_AREA);
-  
-  writeData(GRAPHICS_HOME_ADDRESS & 0xff);
-  writeData(GRAPHICS_HOME_ADDRESS >> 8);  
-
-  writeData(GRAPHICS_AREA);
-  writeData(0x00);
-  writeCommand(SET_GRAPHICS_AREA);   
+  writeCommand(SET_TEXT_AREA);  
   
   // Mode Set
   writeCommand(OR_MODE);
@@ -160,6 +170,8 @@ void initializeDisplay(){
   writeCommand(TEXT_ON_GRAPHIC_ON);
   
   // Test Program
+  clearText();
+  clearGraphic();
   goToText(1, 1);
   display(0x21);
   
