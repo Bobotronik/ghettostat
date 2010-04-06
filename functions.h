aa152b7ab610feba6bc0c1ea2833b6095d28a0b3
robert.alland <robert.alland@57e272f0-0f25-a5b4-1685-e8bbf2b11246>
2010-04-06 04:17:37 +0000

    Added temperature conversions

--- functions.h
+++ functions.h
@@ -1,10 +1,15 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+// Analog to Digital Conversions
 void initADC(void);
 unsigned char convertAD(unsigned char);
 
 // Input must be from 0 to 99
 unsigned char convertDecToBCD(unsigned char);
+
+// Temperature Conversions
+unsigned char convertCtoF(unsigned char *);
+void convertFtoC(unsigned char, unsigned char *);
 
 #endif
