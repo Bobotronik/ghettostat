29e1312999d14e7b0711cab0dbe9b95f7650ddbd
robert.alland <robert.alland@57e272f0-0f25-a5b4-1685-e8bbf2b11246>
2010-04-30 18:09:33 +0000

    Stuff.

--- trunk/int.c
+++ trunk/int.c
@@ -1,6 +1,6 @@
 #include "int.h"
 #include "derivative.h"
-#include "i2cdevices.h"
+#include "i2cDevices.h"
 #include "relayControl.h"
 #include "functions.h"
 #include "hidef.h"
@@ -10,6 +10,7 @@
 
 unsigned char mode;
 unsigned char status;
+unsigned char goalTemp[2] = 0;
 
 #pragma TRAP_PROC
 
@@ -38,7 +39,7 @@ void tempISRAux(void)  {
         clearTempPolarity();      //switch the polarity to 0
       }
     }
-    del1m(1);
+    del1m(10);
 }
 
 #pragma TRAP_PROC
@@ -67,45 +68,53 @@ void tempISRMain(void) {
         clearTempPolarity();      //switch the polarity to 0
       }
     }
-    del1m(1);
+    del1m(10);
 }
 
 #pragma CODE_SEG DEFAULT
 
 void setModeAux(unsigned char * new_settings) {
-  DisableInterrupts;
-  // Turn everything off
-  disableAuxFan();
-  disableAuxCool();
-  disableAuxHeat();
-  setTempC(new_settings);       // Set the desired temp
-  mode = new_settings[2];       // Set the mode
-  status = I_OFF;               
-  if( mode == I_FAN) {
-    enableAuxFan();
-  } else if(mode == I_HEAT) {
-    setTempPolarity();
-  } else {
-    clearTempPolarity();
+  if(new_settings[0] != goalTemp[0] || new_settings[1] != goalTemp[1] 
+      || new_settings[2] != mode) {
+    DisableInterrupts;
+    // Turn everything off
+    disableAuxFan();
+    disableAuxCool();
+    disableAuxHeat();
+    setTempC(new_settings);       // Set the desired temp
+    mode = new_settings[2];       // Set the mode
+    status = I_OFF;
+    goalTemp[0] = new_settings[0];
+    goalTemp[1] = new_settings[1];               
+    if( mode == I_FAN) {
+      enableAuxFan();
+    } else if(mode == I_HEAT) {
+      setTempPolarity();
+    } else {
+      clearTempPolarity();
     }
-  EnableInterrupts;   
+    EnableInterrupts;
+  }   
 }
 
 void setModeMain(unsigned char tempF, unsigned char mode1) {
-  DisableInterrupts;
-  // Turn everything off
-  disableMainFan();
-  disableMainCool();
-  disableMainHeat();
-  setTempF(tempF);       // Set the desired temp
-  mode = mode1;
-  status = I_OFF;             
-  if(mode == I_FAN) {
-    enableMainFan();
-  } else if(mode == I_HEAT){
-    setTempPolarity();
-  } else {
-    clearTempPolarity();
-  }
-  EnableInterrupts;   
+  if( tempF != goalTemp[0] || mode1 != mode) {
+    DisableInterrupts;
+    // Turn everything off
+    disableMainFan();
+    disableMainCool();
+    disableMainHeat();
+    setTempF(tempF);       // Set the desired temp
+    mode = mode1;
+    status = I_OFF;
+    goalTemp[0] = tempF;             
+    if(mode == I_FAN) {
+      enableMainFan();
+    } else if(mode == I_HEAT){
+      setTempPolarity();
+    } else {
+      clearTempPolarity();
+    }
+    EnableInterrupts;
+  }   
 }
